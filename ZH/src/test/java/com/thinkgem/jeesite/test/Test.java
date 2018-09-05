aad4826c0f221f2e7f99e37f5900b5b6491b9cca
王子鹏 <572795308@qq.com>
2018-09-05 10:51:24 +0800

    测试提交

--- ZH/src/test/java/com/thinkgem/jeesite/test/Test.java
+++ ZH/src/test/java/com/thinkgem/jeesite/test/Test.java
@@ -3,6 +3,6 @@ package com.thinkgem.jeesite.test;
 public class Test {
 
 	public static void main(String[] args) {
-		
+		System.out.println("-----------WZP.print-----------1值=" + 1 + "," + "当前类=Test.main()");
 	}
 }
